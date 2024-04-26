141f75129799cdf0c5bc378a914d1d88c1ea5540
Kemi1505 <oluwakemikuteyi1505@gmail.com>
2024-04-26 07:24:22 -0700

    1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,4 +6,7 @@ with urllib.request.urlopen(url) as response:
     x_request_id = response.headers.get('X-Request-Id')
     if x_request_id:
         print(x_request_id)
+    else:
+        print("X-Request-Id not found in the response headers.")
+
 
